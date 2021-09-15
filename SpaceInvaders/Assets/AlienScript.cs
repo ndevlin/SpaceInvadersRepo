908b20c1533fda09083a96ca92188244a6bc61c3
Nathan Devlin <ndevlin@seas.upenn.edu>
2021-09-15 15:00:22 -0400

    Got aliens moving nicely

--- SpaceInvaders/Assets/AlienScript.cs
+++ SpaceInvaders/Assets/AlienScript.cs
@@ -1,22 +1,39 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class AlienScript : MonoBehaviour
 {
+
+    public Vector3 forceVector;
     public int pointValue;
+    public int timer;
 
     // Start is called before the first frame update
     void Start()
     {
         pointValue = 10;
+        timer = 300;
+        forceVector.x = 10.0f;
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        timer++;
+
+        Vector3 currVec = ((float)Math.Sin((float)(timer) / 200.0)) * forceVector;
+
+        GetComponent<Rigidbody>().AddRelativeForce(currVec);
     }
+
+
+    void fixedUpdate()
+    {
+    }
+
 
     public GameObject deathExplosion;
 
