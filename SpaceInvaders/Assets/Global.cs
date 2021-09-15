7b6228c9ae686011953f7bcda939e45116a35476
Nathan Devlin <ndevlin@seas.upenn.edu>
2021-09-15 13:47:14 -0400

    Removed spawning, added 55 aliens

--- SpaceInvaders/Assets/Global.cs
+++ SpaceInvaders/Assets/Global.cs
@@ -4,10 +4,6 @@ using UnityEngine;
 
 public class Global : MonoBehaviour
 {
-    public GameObject objToSpawn;
-    public float timer;
-    public float spawnPeriod;
-    public int numberSpawnedEachPeriod;
     public Vector3 originInScreenCoords;
     public int score;
 
@@ -16,9 +12,6 @@ public class Global : MonoBehaviour
     void Start()
     {
         score = 0;
-        timer = 0;
-        spawnPeriod = 5.0f;
-        numberSpawnedEachPeriod = 3;
         originInScreenCoords = Camera.main.WorldToScreenPoint(new Vector3(0, 0, 0));
     }
 
@@ -26,22 +19,6 @@ public class Global : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        timer += Time.deltaTime;
-
-        if(timer > spawnPeriod)
-        {
-            timer = 0;
-            float width = Screen.width;
-            float height = Screen.height;
-
-            for(int i = 0; i < numberSpawnedEachPeriod; i++)
-            {
-                float horizontalPos = Random.Range(0.0f, width);
-                float verticalPos = Random.Range(0.0f, height);
-                Instantiate(objToSpawn, Camera.main.ScreenToWorldPoint(new Vector3(horizontalPos, verticalPos, originInScreenCoords.z)), Quaternion.identity);
-            }
-        }
-
         if (Input.GetKey("escape"))
         {
             Application.LoadLevel("TitleScreen");
